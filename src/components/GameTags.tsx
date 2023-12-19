4e84f91b865a1e23673e9b6e582f84fdab894582
HK47196 <7722833+HK47196@users.noreply.github.com>
2023-12-19 03:04:04 -0500

    move tag wrapper to its own file

---
+++ src/components/GameTags.tsx
@@ -0,0 +1,40 @@
+import styles from "./Sidebar.module.css";
+import {Genres} from "../types.ts";
+
+interface GameTagsProps {
+	tags: Genres;
+	onTagSelect: (tag: string) => void; // Function to call when a tag is selected
+	selectedGenres: Set<string>;
+}
+
+function GameTags({tags, onTagSelect, selectedGenres}: GameTagsProps) {
+	const keys = Object.keys(tags);
+	keys.sort((a, b) => {
+		if (selectedGenres.has(a) && !selectedGenres.has(b)) {
+			return -1;
+		} else if (!selectedGenres.has(a) && selectedGenres.has(b)) {
+			return 1;
+		}
+		return a.localeCompare(b);
+	});
+	return (
+		<div className={styles.tagSection}>
+			<div className={styles.tagHeader}>Narrow by tag</div>
+			<div className={styles.tagsWrapper}>
+				{keys.map((tag, ix) => (
+					<button
+						key={ix}
+						onClick={() => onTagSelect(tag)}
+						role={"checkbox"}
+						aria-checked={selectedGenres.has(tag)}
+						className={`${styles.tagButton}`}
+					>
+						{tag} ({tags[tag]})
+					</button>
+				))}
+			</div>
+		</div>
+	);
+}
+
+export default GameTags;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import styles from './Sidebar.module.css';
 import {Genres} from "../types.ts";
 import PriceSlider from "./PriceSlider.tsx";
+import GameTags from "./GameTags.tsx";
 
 interface SidebarProps {
 	tags: Genres;
@@ -9,39 +10,14 @@ interface SidebarProps {
 }
 
 function Sidebar({tags, onTagSelect, selectedGenres}: SidebarProps) {
-	const keys = Object.keys(tags);
-	keys.sort((a, b) => {
-		if (selectedGenres.has(a) && !selectedGenres.has(b)) {
-			return -1;
-		} else if (!selectedGenres.has(a) && selectedGenres.has(b)) {
-			return 1;
-		}
-		return a.localeCompare(b);
-	});
 	const handlePriceChange = (maxPrice: number) => {
 		console.log(`Price range selected: ${maxPrice}`);
-		// Update the state or perform actions based on the price change
 	};
 
 	return (
 		<aside className={styles.sidebar}>
 			<PriceSlider maxPrice={100} onPriceChange={handlePriceChange}/>
-			<div className={styles.tagSection}>
-				<div className={styles.tagHeader}>Narrow by tag</div>
-				<div className={styles.tagsWrapper}>
-					{keys.map((tag, ix) => (
-						<button
-							key={ix}
-							onClick={() => onTagSelect(tag)}
-							role={"checkbox"}
-							aria-checked={selectedGenres.has(tag)}
-							className={`${styles.tagButton}`}
-						>
-							{tag} ({tags[tag]})
-						</button>
-					))}
-				</div>
-			</div>
+			<GameTags tags={tags} onTagSelect={onTagSelect} selectedGenres={selectedGenres}/>
 		</aside>
 	);
 }
